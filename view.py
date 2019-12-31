209dc989e2c76e7dd2984cce6fc6df6e7f2a0292
purple <2399636007@qq.com>
2019-12-31 17:28:13 +0800

    创建视图

--- view.py
+++ view.py
@@ -1,4 +1,7 @@
 from django.http import Httpresponse
-
+from django.shortcuts import redirect
 def index(request):
     return Httpresponce('index')
+
+def login(request):
+    return redirect('/index')
